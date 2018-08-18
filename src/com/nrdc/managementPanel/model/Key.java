cd4671b92284fd8d64803fe27c595160f2d941f0
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-18 16:50:10 +0430

    add new model : key

---
+++ src/com/nrdc/managementPanel/model/Key.java
@@ -0,0 +1,75 @@
+package com.nrdc.managementPanel.model;
+
+import com.nrdc.managementPanel.helper.Constants;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "KEYS", schema = Constants.SCHEMA)
+public class Key implements Serializable {
+
+    private Long id;
+    private String key;
+    private Long fkSystemId;
+    private Long fkUserId;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_KEYS")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "KEY", table = "KEYS")
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    @Basic
+    @Column(name = "FK_SYSTEM_ID", table = "KEYS")
+    public Long getFkSystemId() {
+        return fkSystemId;
+    }
+
+    public void setFkSystemId(Long fkSystemId) {
+        this.fkSystemId = fkSystemId;
+    }
+
+    @Basic
+    @Column(name = "FK_USER_ID", table = "KEYS")
+    public Long getFkUserId() {
+        return fkUserId;
+    }
+
+    public void setFkUserId(Long fkUserId) {
+        this.fkUserId = fkUserId;
+    }
+
+    @Override
+    public String toString() {
+        return "Key{" +
+                "id=" + id +
+                ", key='" + key + '\'' +
+                ", fkSystemId=" + fkSystemId +
+                ", fkUserId=" + fkUserId +
+                '}';
+    }
+    public Key(){}
+    public Key (User user, Systems system) throws Exception {
+        user.checkKey(system);
+        this.setKey(UUID.randomUUID().toString());
+        this.setFkSystemId(system.getId());
+        this.setFkUserId(user.getId());
+    }
+}
