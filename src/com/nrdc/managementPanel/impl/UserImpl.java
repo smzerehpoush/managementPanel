e4fedbef73f60782e62fb4d7f5ada91625baaafb
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-18 18:26:50 +0430

    change request and response names

--- src/com/nrdc/managementPanel/impl/UserImpl.java
+++ src/com/nrdc/managementPanel/impl/UserImpl.java
@@ -1,29 +1,34 @@
 package com.nrdc.managementPanel.impl;
 
+import com.nrdc.managementPanel.helper.PrivilegeNames;
 import com.nrdc.managementPanel.helper.SystemNames;
 import com.nrdc.managementPanel.jsonModel.StandardResponse;
-import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActiveUser;
-import com.nrdc.managementPanel.jsonModel.jsonResponse.ResponseActiveUser;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActivateUser;
+import com.nrdc.managementPanel.jsonModel.jsonResponse.ResponseActivateUser;
 import com.nrdc.managementPanel.model.Token;
+import com.nrdc.managementPanel.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 public class UserImpl {
-    public StandardResponse<ResponseActiveUser> activeUser(String token, RequestActiveUser request) throws Exception {
+    public StandardResponse<ResponseActivateUser> activeUser(String token, RequestActivateUser request) throws Exception {
         EntityManager entityManager = Database.getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             Token.validateToken(token, SystemNames.MANAGEMENT_PANEL);
+            User user = User.getUser(token, SystemNames.MANAGEMENT_PANEL);
+            user.checkPrivilege(PrivilegeNames.ACTIVATE_USER);
             if (transaction != null && !transaction.isActive())
                 transaction.begin();
             entityManager.createQuery("UPDATE User u SET u.isActive = true WHERE u.id = (SELECT t.fkUserId FROM Token t WHERE t.token = :token AND t.fkSystemId = (SELECT s.id FROM System s WHERE s.systemName = :systemName))")
                     .setParameter("token", token)
-                    .setParameter("systemName", SystemNames.MANAGEMENT_PANEL);
+                    .setParameter("systemName", SystemNames.MANAGEMENT_PANEL.name())
+                    .executeUpdate();
             if (transaction != null && transaction.isActive())
                 transaction.commit();
 
-            StandardResponse<ResponseActiveUser> response = new StandardResponse<>();
+            StandardResponse<ResponseActivateUser> response = new StandardResponse<>();
             response.setResultCode(1);
             response.setResultMessage("OK");
             return response;
@@ -31,7 +36,7 @@ public class UserImpl {
         } catch (Exception ex) {
             if (transaction != null && transaction.isActive())
                 transaction.rollback();
-            if (entityManager != null && entityManager.isOpen())
+            if (entityManager.isOpen())
                 entityManager.close();
             return StandardResponse.getNOKExceptions(ex);
         }
