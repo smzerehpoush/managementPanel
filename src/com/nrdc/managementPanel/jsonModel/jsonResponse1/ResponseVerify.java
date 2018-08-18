a167335b3b946126330950329668f36842c5d26d
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-18 16:52:30 +0430

    add new Response : ResponseVerify

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonResponse1/ResponseVerify.java
@@ -0,0 +1,40 @@
+package com.nrdc.managementPanel.jsonModel.jsonResponse;
+
+public class ResponseVerify {
+    private int statusCode;
+    private String name;
+    private String status;
+    private String time;
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(int statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
