8d5e47a30cbd8e646793c7b45ae318e4a21efd82
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-18 16:51:24 +0430

    add new Enum : SystemNames

---
+++ src/com/nrdc/managementPanel/helper/SystemNames.java
@@ -0,0 +1,7 @@
+package com.nrdc.managementPanel.helper;
+
+public enum SystemNames {
+    //danger!
+    //do not change this file!!!!
+    VEHICLE_TICKET, NAZER, POLICE_HAMRAH, MANAGEMENT_PANEL
+}
