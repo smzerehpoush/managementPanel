ed499354f22644381530be495f7c5c698f80fa90
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-18 17:19:07 +0430

    add new model : Role

---
+++ src/com/nrdc/managementPanel/model/Role.java
@@ -0,0 +1,59 @@
+package com.nrdc.managementPanel.model;
+
+import com.nrdc.managementPanel.helper.Constants;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "ROLE", schema = Constants.SCHEMA)
+public class Role {
+    private Long id;
+    private String role;
+
+    public Role() {
+    }
+
+    public Role(Long id, String privilege) {
+        this.id = id;
+        this.role = privilege;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_ROLE")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "ROLE")
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String privilege) {
+        this.role = privilege;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Role)) return false;
+
+        Role privilege1 = (Role) o;
+
+        if (!getId().equals(privilege1.getId())) return false;
+        return getRole().equals(privilege1.getRole());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId().hashCode();
+        result = 31 * result + getRole().hashCode();
+        return result;
+    }
+}
