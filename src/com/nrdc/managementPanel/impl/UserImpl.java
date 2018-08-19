995f17ec1aca07d1f429a92d902d6828d6995b93
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-19 11:36:15 +0430

    implement new service : addUser

--- src/com/nrdc/managementPanel/impl/UserImpl.java
+++ src/com/nrdc/managementPanel/impl/UserImpl.java
@@ -4,7 +4,8 @@ import com.nrdc.managementPanel.helper.PrivilegeNames;
 import com.nrdc.managementPanel.helper.SystemNames;
 import com.nrdc.managementPanel.jsonModel.StandardResponse;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActivateUser;
-import com.nrdc.managementPanel.jsonModel.jsonResponse.ResponseActivateUser;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestAddUser;
+import com.nrdc.managementPanel.model.System;
 import com.nrdc.managementPanel.model.Token;
 import com.nrdc.managementPanel.model.User;
 
@@ -38,6 +39,35 @@ public class UserImpl {
                 transaction.rollback();
             if (entityManager.isOpen())
                 entityManager.close();
+            return StandardResponse.getNOKExceptions(ex);
+        } finally {
+            if (entityManager.isOpen())
+                entityManager.close();
+        }
+    }
+
+    public StandardResponse addUser(String token, RequestAddUser requestAddUser) {
+        EntityManager entityManager = Database.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            Token.validateToken(token, SystemNames.MANAGEMENT_PANEL);
+            User user = User.getUser(token, SystemNames.MANAGEMENT_PANEL);
+            System system = System.getSystem(requestAddUser.getFkSystemId());
+            String privilegeName = "ADD_"+system.getSystemName()+"_USER";
+            user.checkPrivilege(privilegeName);
+            if (!transaction.isActive())
+                transaction.begin();
+            User u = new User(requestAddUser);
+            entityManager.persist(u);
+            if (transaction.isActive())
+                transaction.commit();
+            StandardResponse response = new StandardResponse<>();
+            response.setResultCode(1);
+            response.setResultMessage("OK");
+            return response;
+        } catch (Exception ex) {
+            if (transaction != null && transaction.isActive())
+                transaction.rollback();
             return StandardResponse.getNOKExceptions(ex);
         } finally {
             if (entityManager.isOpen())
