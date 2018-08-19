3a143146853886da88f66f5c2cd61743342c8b1c
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-19 11:42:39 +0430

    add new request : deActiveUser

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestDeActiveUser.java
@@ -0,0 +1,30 @@
+package com.nrdc.managementPanel.jsonModel.jsonRequest;
+
+public class RequestDeActiveUser {
+    private Long fkUserId;
+    private Long fkSystemId;
+
+    public Long getFkUserId() {
+        return fkUserId;
+    }
+
+    public void setFkUserId(Long fkUserId) {
+        this.fkUserId = fkUserId;
+    }
+
+    public Long getFkSystemId() {
+        return fkSystemId;
+    }
+
+    public void setFkSystemId(Long fkSystemId) {
+        this.fkSystemId = fkSystemId;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestActiveUser{" +
+                "fkUserId=" + fkUserId +
+                ", fkSystemId=" + fkSystemId +
+                '}';
+    }
+}
