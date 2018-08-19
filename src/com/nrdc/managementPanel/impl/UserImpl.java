5b404dfcab1c629b621f82d0895e46d1f3a91eeb
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-19 13:48:28 +0430

    implement new service : getUsers

--- src/com/nrdc/managementPanel/impl/UserImpl.java
+++ src/com/nrdc/managementPanel/impl/UserImpl.java
@@ -6,12 +6,15 @@ import com.nrdc.managementPanel.jsonModel.StandardResponse;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActiveUser;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestAddUser;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestDeActiveUser;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestGetUsers;
+import com.nrdc.managementPanel.jsonModel.jsonResponse.ResponseGetUsers;
 import com.nrdc.managementPanel.model.System;
 import com.nrdc.managementPanel.model.Token;
 import com.nrdc.managementPanel.model.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import java.util.List;
 
 public class UserImpl {
     public StandardResponse activeUser (String token, RequestActiveUser request) throws Exception {
@@ -102,6 +105,32 @@ public class UserImpl {
             if (transaction != null && transaction.isActive())
                 transaction.rollback();
             return StandardResponse.getNOKExceptions(ex);
+        } finally {
+            if (entityManager.isOpen())
+                entityManager.close();
+        }
+    }
+    public StandardResponse getUsers(String token, RequestGetUsers requestAddUser) throws Exception {
+        EntityManager entityManager = Database.getEntityManager();
+        try {
+            Token.validateToken(token, SystemNames.MANAGEMENT_PANEL);
+            User user = User.getUser(token, SystemNames.MANAGEMENT_PANEL);
+            System system = System.getSystem(requestAddUser.getFkSystemId());
+            String privilegeName = "GET_"+system.getSystemName()+"_USERS";
+            user.checkPrivilege(privilegeName);
+            List<User> users = entityManager.createQuery("SELECT u FROM User u JOIN UserSystem us ON u.id = us.fkUserId WHERE us.fkSystemId = :fkSystemId")
+                    .setParameter("fkSystemId",system.getId())
+                    .getResultList();
+            for (User u : users){
+                u.setPassword(null);
+            }
+            ResponseGetUsers responseGetUsers = new ResponseGetUsers();
+            responseGetUsers.setUsers(users);
+            StandardResponse response = new StandardResponse<>();
+            response.setResultCode(1);
+            response.setResultMessage("OK");
+            response.setResponse(responseGetUsers);
+            return response;
         } finally {
             if (entityManager.isOpen())
                 entityManager.close();
