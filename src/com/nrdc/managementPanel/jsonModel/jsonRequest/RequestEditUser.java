aa4283cd7760a301cd093809e3ca8e0f57018802
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-19 17:13:02 +0430

    add new Request : requestEditUser

--- src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestEditUser.java
+++ src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestEditUser.java
@@ -1,4 +1,77 @@
 package com.nrdc.managementPanel.jsonModel.jsonRequest;
 
+
 public class RequestEditUser {
+    private Long id;
+    private String username;
+    private Boolean isActive;
+    private String phoneNumber;
+    private String firstName;
+    private String lastName;
+    private String nationalId;
+    private String policeCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationalId() {
+        return nationalId;
+    }
+
+    public void setNationalId(String nationalId) {
+        this.nationalId = nationalId;
+    }
+
+    public String getPoliceCode() {
+        return policeCode;
+    }
+
+    public void setPoliceCode(String policeCode) {
+        this.policeCode = policeCode;
+    }
 }
