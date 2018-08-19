6b92afb990c0b8e8ce53ddec7a33c8e1b56640ae
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-19 13:47:54 +0430

    adding new response : responseGetUsers

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonResponse/ResponseGetUsers.java
@@ -0,0 +1,17 @@
+package com.nrdc.managementPanel.jsonModel.jsonResponse;
+
+import com.nrdc.managementPanel.model.User;
+
+import java.util.List;
+
+public class ResponseGetUsers {
+    private List<User> users ;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
