c5203cf15ff6700a05b2cbd1f5762c35152dde61
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-19 11:59:59 +0430

    implement new service : deActiveUser

--- src/com/nrdc/managementPanel/impl/UserImpl.java
+++ src/com/nrdc/managementPanel/impl/UserImpl.java
@@ -5,6 +5,7 @@ import com.nrdc.managementPanel.helper.SystemNames;
 import com.nrdc.managementPanel.jsonModel.StandardResponse;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActiveUser;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestAddUser;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestDeActiveUser;
 import com.nrdc.managementPanel.model.System;
 import com.nrdc.managementPanel.model.Token;
 import com.nrdc.managementPanel.model.User;
@@ -13,7 +14,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
 public class UserImpl {
-    public StandardResponse activeUser(String token, RequestActiveUser request) throws Exception {
+    public StandardResponse activeUser (String token, RequestActiveUser request) throws Exception {
         EntityManager entityManager = Database.getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         try {
@@ -23,6 +24,38 @@ public class UserImpl {
             if (transaction != null && !transaction.isActive())
                 transaction.begin();
             entityManager.createQuery("UPDATE User u SET u.isActive = true WHERE u.id = (SELECT t.fkUserId FROM Token t WHERE t.token = :token AND t.fkSystemId = (SELECT s.id FROM System s WHERE s.systemName = :systemName))")
+                    .setParameter("token", token)
+                    .setParameter("systemName", SystemNames.MANAGEMENT_PANEL.name())
+                    .executeUpdate();
+            if (transaction != null && transaction.isActive())
+                transaction.commit();
+
+            StandardResponse response = new StandardResponse<>();
+            response.setResultCode(1);
+            response.setResultMessage("OK");
+            return response;
+
+        } catch (Exception ex) {
+            if (transaction != null && transaction.isActive())
+                transaction.rollback();
+            if (entityManager.isOpen())
+                entityManager.close();
+            return StandardResponse.getNOKExceptions(ex);
+        } finally {
+            if (entityManager.isOpen())
+                entityManager.close();
+        }
+    }
+    public StandardResponse deActiveUser (String token, RequestDeActiveUser request) throws Exception {
+        EntityManager entityManager = Database.getEntityManager();
+        EntityTransaction transaction = entityManager.getTransaction();
+        try {
+            Token.validateToken(token, SystemNames.MANAGEMENT_PANEL);
+            User user = User.getUser(token, SystemNames.MANAGEMENT_PANEL);
+            user.checkPrivilege(PrivilegeNames.ACTIVATE_USER);
+            if (transaction != null && !transaction.isActive())
+                transaction.begin();
+            entityManager.createQuery("UPDATE User u SET u.isActive = false WHERE u.id = (SELECT t.fkUserId FROM Token t WHERE t.token = :token AND t.fkSystemId = (SELECT s.id FROM System s WHERE s.systemName = :systemName))")
                     .setParameter("token", token)
                     .setParameter("systemName", SystemNames.MANAGEMENT_PANEL.name())
                     .executeUpdate();
