3d06f6ec6a64bd92ecdc23381905380f91e6c939
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-20 14:00:56 +0430

    add new Customized model : RolesWithPrivileges

---
+++ src/com/nrdc/managementPanel/jsonModel/customizedModel/RoleWithPrivileges.java
@@ -0,0 +1,35 @@
+package com.nrdc.managementPanel.jsonModel.customizedModel;
+
+import com.nrdc.managementPanel.model.Privilege;
+
+import java.util.List;
+
+public class RoleWithPrivileges {
+    private Long id;
+    private String role;
+    private List<Privilege> privileges;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public List<Privilege> getPrivileges() {
+        return privileges;
+    }
+
+    public void setPrivileges(List<Privilege> privileges) {
+        this.privileges = privileges;
+    }
+}
