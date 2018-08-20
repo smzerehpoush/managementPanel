38038e1e448c58374050c952db9f7f327079c2ee
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-20 17:16:55 +0430

    add new request : RequesetRemoveToken

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestRemoveToken.java
@@ -0,0 +1,22 @@
+package com.nrdc.managementPanel.jsonModel.jsonRequest;
+
+public class RequestRemoveToken {
+    private String token;
+    private Long fkSystemId;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Long getFkSystemId() {
+        return fkSystemId;
+    }
+
+    public void setFkSystemId(Long fkSystemId) {
+        this.fkSystemId = fkSystemId;
+    }
+}
