ec735e42d7f4294270ee67ac5d7008f9ef061066
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-20 14:25:04 +0430

    add new response : responseGetRoles

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonResponse/ResponseGetRoles.java
@@ -0,0 +1,17 @@
+package com.nrdc.managementPanel.jsonModel.jsonResponse;
+
+import com.nrdc.managementPanel.model.Role;
+
+import java.util.List;
+
+public class ResponseGetRoles {
+    private List<Role> roles ;
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+}
