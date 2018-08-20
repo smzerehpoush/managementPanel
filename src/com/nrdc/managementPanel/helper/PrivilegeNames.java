059dc5d747d073c897643fa9d59af24fe178c591
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-20 16:39:41 +0430

    add new privilege : GET_SYSTEM

--- src/com/nrdc/managementPanel/helper/PrivilegeNames.java
+++ src/com/nrdc/managementPanel/helper/PrivilegeNames.java
@@ -1,5 +1,5 @@
 package com.nrdc.managementPanel.helper;
 
 public enum PrivilegeNames {
-    ACTIVATE_USER,FILTER_USERS,ADD_USERS, GET_PRIVILEGES, GET_ROLES
+    ACTIVATE_USER,FILTER_USERS,ADD_USERS, GET_PRIVILEGES, GET_ROLES, GET_SYSTEMS
 }
