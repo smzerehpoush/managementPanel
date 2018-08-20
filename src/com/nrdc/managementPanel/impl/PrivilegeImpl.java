cfe6fbe2f84b9f17ec34932f7ed589f25519ad4a
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-20 14:24:36 +0430

    move method to another class

--- src/com/nrdc/managementPanel/impl/PrivilegeImpl.java
+++ src/com/nrdc/managementPanel/impl/PrivilegeImpl.java
@@ -33,27 +33,4 @@ public class PrivilegeImpl {
         }
     }
 
-    public StandardResponse getPrivileges(String token, Long fkRoleId) throws Exception {
-        EntityManager entityManager = Database.getEntityManager();
-        try {
-            Token.validateToken(token, SystemNames.MANAGEMENT_PANEL);
-            User user = User.getUser(token, SystemNames.MANAGEMENT_PANEL);
-            user.checkPrivilege(PrivilegeNames.GET_PRIVILEGES);
-            List privileges = entityManager.createQuery("SELECT p FROM Privilege p JOIN RolePrivilege rp ON p.id = rp.fkPrivilegeId WHERE rp.fkRoleId = :fkRoleId")
-                    .setParameter("fkRoleId", fkRoleId)
-                    .getResultList();
-            ResponseGetPrivileges responseGetPrivileges = new ResponseGetPrivileges();
-            responseGetPrivileges.setPrivileges(privileges);
-            StandardResponse response = new StandardResponse<>();
-            response.setResultCode(1);
-            response.setResultMessage("OK");
-            response.setResponse(responseGetPrivileges);
-            return response;
-        } finally {
-            if (entityManager.isOpen())
-                entityManager.close();
-        }
-    }
-
-
 }
