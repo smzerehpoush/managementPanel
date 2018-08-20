c4752ce5d6d2c9b78821e282e54b3cdab92a3e98
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-08-20 14:01:20 +0430

    add new Request : getUserRolesWithPrivileges

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestGetUserRolesWithPrivileges.java
@@ -0,0 +1,13 @@
+package com.nrdc.managementPanel.jsonModel.jsonRequest;
+
+public class RequestGetUserRolesWithPrivileges {
+    private Long fkUserId;
+
+    public Long getFkUserId() {
+        return fkUserId;
+    }
+
+    public void setFkUserId(Long fkUserId) {
+        this.fkUserId = fkUserId;
+    }
+}
