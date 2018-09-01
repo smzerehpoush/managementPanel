2b5f34e1a4069d5aaae353378f7cebba699cb184
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-01 15:14:36 +0430

    add new model : Operation

---
+++ src/com/nrdc/managementPanel/model/Operation.java
@@ -0,0 +1,68 @@
+package com.nrdc.managementPanel.model;
+
+import com.nrdc.managementPanel.helper.Constants;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "OPERATION", schema = Constants.SCHEMA)
+public class Operation {
+    private Long id;
+    private Long fkUserId;
+    private Long fkPrivilegeId;
+    private Date time;
+    private String description;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_OPERATION")
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "FK_USER_ID")
+    public Long getFkUserId() {
+        return fkUserId;
+    }
+
+    public void setFkUserId(Long fkUserId) {
+        this.fkUserId = fkUserId;
+    }
+
+    @Basic
+    @Column(name = "FK_PRIVILEGE_ID")
+    public Long getFkPrivilegeId() {
+        return fkPrivilegeId;
+    }
+
+    public void setFkPrivilegeId(Long fkPrivilegeId) {
+        this.fkPrivilegeId = fkPrivilegeId;
+    }
+
+    @Basic
+    @Column(name = "TIME")
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    @Basic
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
