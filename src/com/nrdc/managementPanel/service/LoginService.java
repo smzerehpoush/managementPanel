13fdca7e81aab984f34be8a093e747f9db13a349
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-02 18:01:47 +0430

    change login service encryption and sending response

--- src/com/nrdc/managementPanel/service/LoginService.java
+++ src/com/nrdc/managementPanel/service/LoginService.java
@@ -1,13 +1,7 @@
 package com.nrdc.managementPanel.service;
 
-import com.nrdc.managementPanel.helper.Encryption;
-import com.nrdc.managementPanel.impl.Database;
 import com.nrdc.managementPanel.impl.LoginImpl;
-import com.nrdc.managementPanel.impl.UserImpl;
-import com.nrdc.managementPanel.jsonModel.EncryptedRequest;
-import com.nrdc.managementPanel.jsonModel.EncryptedResponse;
 import com.nrdc.managementPanel.jsonModel.StandardResponse;
-import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActiveUser;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestLogin;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -24,18 +18,14 @@ public class LoginService {
     private static Logger logger = Logger.getLogger(UserServices.class.getName());
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @Path("/login")
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response login(EncryptedRequest encryptedRequest){
+    public Response login(RequestLogin requestLogin) {
         logger.info("++================== activeUser SERVICE : START ==================++");
         try {
-            RequestLogin request = objectMapper.readValue(Encryption.decryptRequest(encryptedRequest), RequestLogin.class);
-            StandardResponse response = new LoginImpl().login(encryptedRequest.getToken(), request);
-            String key = Database.getUserKey(encryptedRequest.getToken()).getKey();
-            EncryptedResponse encryptedResponse = Encryption.encryptResponse(key, response);
-            Response finalResponse = Response.status(200).entity(encryptedResponse).build();
+            StandardResponse response = new LoginImpl().login(requestLogin);
+            Response finalResponse = Response.status(200).entity(response).build();
             logger.info("++================== activeUser SERVICE : END ==================++");
             return finalResponse;
         } catch (Exception ex) {
