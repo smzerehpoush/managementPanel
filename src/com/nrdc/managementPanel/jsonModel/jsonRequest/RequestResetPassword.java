b7f3ac9e57f51a534a3a1f593991bb366c167793
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-02 09:41:08 +0430

    add new request : request ResetPassword
    String username, String oldPassword, String newPassword

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestResetPassword.java
@@ -0,0 +1,40 @@
+package com.nrdc.managementPanel.jsonModel.jsonRequest;
+
+public class RequestResetPassword {
+    private String username;
+    private String oldPassword;
+    private String newPassword;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "RequestResetPassword{" +
+                "username='" + username + '\'' +
+                ", oldPassword='" + oldPassword + '\'' +
+                ", newPassword='" + newPassword + '\'' +
+                '}';
+    }
+}
