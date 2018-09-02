98a1a30aee88e4e317fad8801f1f856750cbc4b3
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-02 18:29:53 +0430

    implement new method : getUserPassword

--- src/com/nrdc/managementPanel/impl/LoginImpl.java
+++ src/com/nrdc/managementPanel/impl/LoginImpl.java
@@ -5,6 +5,19 @@ import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestLogin;
 
 public class LoginImpl {
     public StandardResponse login(String token, RequestLogin requestLogin){
+    private String getUserPassword(String username) throws Exception {
+        EntityManager entityManager = Database.getEntityManager();
+        try {
+            return (String) entityManager.createQuery("SELECT u.password FROM User u WHERE u.username = :username")
+                    .setParameter("username", username)
+                    .getSingleResult();
+
+        } catch (Exception ex) {
+            throw new Exception(Constants.INCORRECT_USERNAME_OR_PASSWORD);
+        } finally {
+            if (entityManager != null && entityManager.isOpen())
+                entityManager.close();
+        }
 
     }
 }
