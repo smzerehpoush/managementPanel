3bd729b984560f5d7c59074a2d5b9903b8281511
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-02 15:23:55 +0430

    add new impl : login impl

---
+++ src/com/nrdc/managementPanel/impl/LoginImpl.java
@@ -0,0 +1,10 @@
+package com.nrdc.managementPanel.impl;
+
+import com.nrdc.managementPanel.jsonModel.StandardResponse;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestLogin;
+
+public class LoginImpl {
+    public StandardResponse login(String token, RequestLogin requestLogin){
+
+    }
+}
