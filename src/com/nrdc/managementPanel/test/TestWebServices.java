d2c3f009b149ef95e9f53e5e721c394234974017
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-03 11:28:28 +0430

    add test class

---
+++ src/com/nrdc/managementPanel/test/TestWebServices.java
@@ -0,0 +1,106 @@
+package com.nrdc.managementPanel.test;
+
+import com.nrdc.managementPanel.helper.Encryption;
+import com.nrdc.managementPanel.jsonModel.EncryptedRequest;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestActiveUser;
+import org.apache.log4j.Logger;
+import org.codehaus.jackson.map.ObjectMapper;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+@Path("/test")
+public class TestWebServices {
+    private final static String baseUrl = "http://127.0.0.1:7001/managementPanel/api";
+    private static Logger logger = Logger.getLogger(TestWebServices.class.getName());
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response test() {
+        StringBuilder stringBuilder = new StringBuilder();
+        String testActivateUserServiceResult = testActivateUserService();
+        stringBuilder.append(testActivateUserServiceResult);
+        Response finalResponse = Response.status(200).entity(stringBuilder.toString()).build();
+        return finalResponse;
+    }
+
+    private String testActivateUserService() {
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("Test Activate User Service");
+            RequestActiveUser requestActiveUser = new RequestActiveUser();
+            requestActiveUser.setFkUserId(1L);
+            String response = sendPostRequest(baseUrl + "/user/activate", requestActiveUser, "key", "token");
+            logger.info(response);
+            stringBuilder.append(response);
+            return stringBuilder.toString();
+        } catch (Exception ex) {
+            return null;
+        }
+
+    }
+
+    private String sendPostRequest(String path, Object request, String key, String token) {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            EncryptedRequest encryptedRequest = new EncryptedRequest();
+            encryptedRequest.setToken(token);
+            String data = Encryption.encryptOrNull(key, objectMapper.writeValueAsString(request));
+            encryptedRequest.setData(data);
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            String input = new ObjectMapper().writeValueAsString(encryptedRequest);
+            OutputStream outputStream = connection.getOutputStream();
+            outputStream.write(input.getBytes());
+            outputStream.flush();
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new Exception("Error in Calling rest service");
+            }
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            int c;
+            while ((c = bufferedReader.read()) != -1)
+                output.append((char) c);
+            String result = output.toString();
+
+            return Encryption.decryptRequest(result, key);
+
+        } catch (Exception ex) {
+            return null;
+        }
+
+    }
+
+    private String sendGetRequest(String path) {
+        try {
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoOutput(true);
+            connection.setRequestMethod("GET");
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new Exception("Error in Calling rest service");
+            }
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder output = new StringBuilder();
+            int c;
+            while ((c = bufferedReader.read()) != -1)
+                output.append((char) c);
+            return output.toString();
+
+        } catch (Exception ex) {
+            return null;
+        }
+
+    }
+}
