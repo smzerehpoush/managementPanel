2d3ddfc4cc3d58a7fd622366df75fba51c5b85a1
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-04 11:30:34 +0430

    add correct UTF8 characters

--- src/com/nrdc/managementPanel/helper/Encryption.java
+++ src/com/nrdc/managementPanel/helper/Encryption.java
@@ -41,15 +41,169 @@ public class Encryption {
         return encryptionBuilder.build();
     }
 
+    private static String reverseUTF8String(String oldData) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char c : oldData.toCharArray()) {
+            switch (c) {
+                case 'À':
+                    stringBuilder.append('ا');
+                    break;
+
+                case 'Á':
+                    stringBuilder.append('ب');
+                    break;
+
+                case 'Â':
+                    stringBuilder.append('ة');
+                    break;
+
+                case 'Ã':
+                    stringBuilder.append('ت');
+                    break;
+
+                case 'Ä':
+                    stringBuilder.append('ث');
+                    break;
+
+                case 'Å':
+                    stringBuilder.append('ج');
+                    break;
+
+                case 'Æ':
+                    stringBuilder.append('ح');
+                    break;
+
+                case 'Ç':
+                    stringBuilder.append('خ');
+                    break;
+
+                case 'È':
+                    stringBuilder.append('د');
+                    break;
+
+                case 'É':
+                    stringBuilder.append('ذ');
+                    break;
+
+                case 'Ê':
+                    stringBuilder.append('ر');
+                    break;
+
+                case 'Ë':
+                    stringBuilder.append('ز');
+                    break;
+
+                case 'Ì':
+                    stringBuilder.append('س');
+                    break;
+
+                case 'Í':
+                    stringBuilder.append('ش');
+                    break;
+
+                case 'Î':
+                    stringBuilder.append('ص');
+                    break;
+
+                case 'Ï':
+                    stringBuilder.append('ض');
+                    break;
+
+                case 'Ð':
+                    stringBuilder.append('ط');
+                    break;
+
+                case 'Ñ':
+                    stringBuilder.append('ظ');
+                    break;
+
+                case 'Ò':
+                    stringBuilder.append('ع');
+                    break;
+
+                case 'Ó':
+                    stringBuilder.append('غ');
+                    break;
+
+                case 'Ô':
+                    stringBuilder.append('ػ');
+                    break;
+
+                case 'Õ':
+                    stringBuilder.append('ؼ');
+                    break;
+
+                case 'Ö':
+                    stringBuilder.append('ؽ');
+                    break;
+
+                case '×':
+                    stringBuilder.append('ؾ');
+                    break;
+
+                case 'Ø':
+                    stringBuilder.append('ؿ');
+                    break;
+
+                case 'Ú':
+                    stringBuilder.append('ف');
+                    break;
+
+                case 'Û':
+                    stringBuilder.append('ق');
+                    break;
+
+                case 'Ü':
+                    stringBuilder.append('ك');
+                    break;
+
+                case 'Ý':
+                    stringBuilder.append('ل');
+                    break;
+
+                case 'Þ':
+                    stringBuilder.append('م');
+                    break;
+
+                case 'ß':
+                    stringBuilder.append('ن');
+                    break;
+
+                case 'à':
+                    stringBuilder.append('ه');
+                    break;
+
+                case 'á':
+                    stringBuilder.append('و');
+                    break;
+
+                case 'â':
+                    stringBuilder.append('ى');
+                    break;
+
+                case 'ã':
+                    stringBuilder.append('ي');
+                    break;
+                default:
+                    stringBuilder.append(c);
+                    break;
+
+            }
+        }
+        return stringBuilder.toString();
+    }
+
     public static String encryptOrNull(String key, String data) throws NoSuchAlgorithmException {
         encryption = getEncryption(key);
         assert encryption != null;
-        return encryption.encryptOrNull(data);
+//        data = correctUTF8String(data);
+        return encryption.encryptOrNull(data).replace("\n", "");
     }
 
     public static String decryptOrNull(String key, String data) throws NoSuchAlgorithmException {
         encryption = getEncryption(key);
         assert encryption != null;
+//        return reverseUTF8String(encryption.decryptOrNull(data));
         return encryption.decryptOrNull(data);
     }
 
