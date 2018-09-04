11829f86e42fff4ef1e69f8db3d1f42f265972b5
Mahdiyar <mahdiyar.zerehpoush@gmail.com>
2018-09-04 11:37:17 +0430

    add test method

--- test/Test.java
+++ test/Test.java
@@ -1,4 +1,16 @@
+import java.io.IOException;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        for (int i = 0; i < 36; i++) {
+            System.out.println(
+                    "case '" + (char) ('ا' + i) + "' : \n" +
+                            "                    stringBuilder.append( '" + (char) ('À' + i) + "' );\n" +
+                            "                break; \n"+
+                            "                ");
+        }
+//        for (int i = 0 ; i <100 ; i++){
+//            System.out.println((char)('z'+i));
+//        }
     }
 }
