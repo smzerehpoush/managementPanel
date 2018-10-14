253f523a0d6e93b54308b09272e86440ab283829
Unknown <unknown@example.com>
2018-10-14 17:04:20 +0330

    add new request : request Add token key

---
+++ src/com/nrdc/managementPanel/jsonModel/jsonRequest/RequestAddTokenKey.java
@@ -0,0 +1,38 @@
+package com.nrdc.managementPanel.jsonModel.jsonRequest;
+
+public class RequestAddTokenKey {
+    private String token;
+    private String key;
+    private Long fkUserId;
+
+    public RequestAddTokenKey(){}
+    public RequestAddTokenKey(String token, String key, Long fkUserId) {
+        this.token = token;
+        this.key = key;
+        this.fkUserId = fkUserId;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Long getFkUserId() {
+        return fkUserId;
+    }
+
+    public void setFkUserId(Long fkUserId) {
+        this.fkUserId = fkUserId;
+    }
+}
