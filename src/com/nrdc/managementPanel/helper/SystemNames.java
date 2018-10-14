7a78d9c83e4d5496f3b9610a309638a4b63b9d40
Unknown <unknown@example.com>
2018-10-14 16:56:53 +0330

    remove MANAGEMENT_PANEL from system names

--- src/com/nrdc/managementPanel/helper/SystemNames.java
+++ src/com/nrdc/managementPanel/helper/SystemNames.java
@@ -3,5 +3,5 @@ package com.nrdc.managementPanel.helper;
 public enum SystemNames {
     //danger!
     //do not change this file!!!!
-    VEHICLE_TICKET, NAZER, POLICE_HAMRAH, MANAGEMENT_PANEL
+    VEHICLE_TICKET, NAZER, POLICE_HAMRAH
 }
