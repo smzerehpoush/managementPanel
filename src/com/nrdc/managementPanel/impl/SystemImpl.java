8f87644647c8762bdbb0c424484599369b053167
Unknown <unknown@example.com>
2018-10-14 17:03:58 +0330

    add new method to ger systems of a user

--- src/com/nrdc/managementPanel/impl/SystemImpl.java
+++ src/com/nrdc/managementPanel/impl/SystemImpl.java
@@ -4,6 +4,7 @@ import com.nrdc.managementPanel.helper.PrivilegeNames;
 import com.nrdc.managementPanel.helper.SystemNames;
 import com.nrdc.managementPanel.jsonModel.StandardResponse;
 import com.nrdc.managementPanel.jsonModel.jsonResponse.ResponseGetSystems;
+import com.nrdc.managementPanel.model.dao.System;
 import com.nrdc.managementPanel.model.dao.User;
 
 import javax.persistence.EntityManager;
@@ -26,6 +27,31 @@ public class SystemImpl {
             return response;
         } finally {
             if (entityManager.isOpen())
+                entityManager.close();
+        }
+    }
+    public StandardResponse<ResponseGetSystems> getSystems(Long fkUserId) {
+        EntityManager entityManager = Database.getEntityManager();
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.append("SELECT s FROM Systems s ");
+            if (fkUserId != null) {
+                stringBuilder.append("JOIN UserSystem u on s.id = u.fkSystemId WHERE u.fkUserId = ");
+                stringBuilder.append(fkUserId);
+            }
+            String query = stringBuilder.toString();
+            List<System> systems = entityManager.createQuery(query).getResultList();
+            ResponseGetSystems responseGetSystems = new ResponseGetSystems();
+            responseGetSystems.setSystems(systems);
+            StandardResponse<ResponseGetSystems> response = new StandardResponse<>();
+            response.setResponse(responseGetSystems);
+            response.setResultCode(1);
+            response.setResultMessage("OK");
+            return response;
+        } catch (Exception ex) {
+            return new StandardResponse<ResponseGetSystems>().getNOKExceptions(ex);
+        }finally {
+            if (entityManager != null && entityManager.isOpen())
                 entityManager.close();
         }
     }
