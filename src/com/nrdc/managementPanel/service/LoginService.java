2c16712ef3e0e2f0087bccf07bc1f9b91feecad7
Unknown <unknown@example.com>
2018-10-14 17:24:17 +0330

    add login to system service

--- src/com/nrdc/managementPanel/service/LoginService.java
+++ src/com/nrdc/managementPanel/service/LoginService.java
@@ -1,8 +1,14 @@
 package com.nrdc.managementPanel.service;
 
+import com.nrdc.managementPanel.helper.Encryption;
+import com.nrdc.managementPanel.impl.Database;
 import com.nrdc.managementPanel.impl.LoginImpl;
+import com.nrdc.managementPanel.jsonModel.EncryptedRequest;
+import com.nrdc.managementPanel.jsonModel.EncryptedResponse;
 import com.nrdc.managementPanel.jsonModel.StandardResponse;
 import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestLogin;
+import com.nrdc.managementPanel.jsonModel.jsonRequest.RequestLoginToSystems;
+import com.nrdc.managementPanel.model.dao.User;
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -33,11 +39,26 @@ public class LoginService {
             return Response.status(200).entity(response).build();
         }
     }
+    @Path("/system")
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response login(EncryptedRequest request) {
+        logger.info("++================== login-to-system SERVICE : START ==================++");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            RequestLoginToSystems requestLogin = objectMapper.readValue(Encryption.decryptRequest(request), RequestLoginToSystems.class);
+            StandardResponse response = new LoginImpl().loginToSystem(request.getToken(), requestLogin);
+            String key = User.getKey(request.getToken()).getKey();
+            EncryptedResponse encryptedResponse = Encryption.encryptResponse(key, response);
+            Response finalResponse = Response.status(200).entity(encryptedResponse).build();
+            logger.info("++================== login-to-system SERVICE : END ==================++");
             return finalResponse;
         } catch (Exception ex) {
-            logger.error("++================== activeUser SERVICE : EXCEPTION ==================++");
+            logger.error("++================== login-to-system SERVICE : EXCEPTION ==================++");
             StandardResponse response = StandardResponse.getNOKExceptions(ex);
             return Response.status(200).entity(response).build();
         }
     }
+
 }
