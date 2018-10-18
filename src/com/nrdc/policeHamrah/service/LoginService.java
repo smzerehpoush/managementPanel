9bb520ba636bf2fb70f4c9cadacdf00caec364ae
Unknown <unknown@example.com>
2018-10-18 16:52:58 +0330

    add done flag

--- src/com/nrdc/policeHamrah/service/LoginService.java
+++ src/com/nrdc/policeHamrah/service/LoginService.java
@@ -17,7 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+//done
 @Path("/login")
 public class LoginService {
     private static Logger logger = Logger.getLogger(LoginService.class.getName());
--- src/com/nrdc/policeHamrah/service/LogoutService.java
+++ src/com/nrdc/policeHamrah/service/LogoutService.java
@@ -14,7 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
+//done
 @Path("/logout")
 public class LogoutService {
     private static Logger logger = Logger.getLogger(LogoutService.class.getName());
