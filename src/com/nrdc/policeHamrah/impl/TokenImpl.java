3ec192d4c87beaad458bbfcc77be9e6e504484e4
Unknown <unknown@example.com>
2018-10-18 17:30:22 +0330

    change service implementation

--- src/com/nrdc/policeHamrah/impl/TokenImpl.java
+++ src/com/nrdc/policeHamrah/impl/TokenImpl.java
@@ -1,10 +1,11 @@
 package com.nrdc.policeHamrah.impl;
 
 import com.nrdc.policeHamrah.helper.Constants;
+import com.nrdc.policeHamrah.helper.PrivilegeNames;
 import com.nrdc.policeHamrah.jsonModel.StandardResponse;
 import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestRemoveToken;
+import com.nrdc.policeHamrah.model.dao.PrivilegeDao;
 import com.nrdc.policeHamrah.model.dao.SystemDao;
-import com.nrdc.policeHamrah.model.dao.TokenDao;
 import com.nrdc.policeHamrah.model.dao.UserDao;
 
 import javax.persistence.EntityManager;
@@ -16,23 +17,16 @@ public class TokenImpl {
         EntityTransaction transaction = entityManager.getTransaction();
         try {
             UserDao user = UserDao.validate(token);
-            SystemDao us = SystemDao.getSystem(requestRemoveToken.getFkSystemId());
-            TokenDao.validateToken(requestRemoveToken.getToken(), us.getSystemName());
-
-            Long fkUserId = (Long) entityManager.createQuery("SELECT t.fkUserId FROM TokenDao t WHERE t.token = :token AND t.fkSystemId = :fkSystemId")
-                    .setParameter("token", requestRemoveToken.getToken())
-                    .setParameter("fkSystemId", requestRemoveToken.getFkSystemId())
-                    .getSingleResult();
-            if (user.getId().equals(fkUserId)) {
-                throw new Exception(Constants.CAN_NOT_REMOVE_YOUR_TOKEN);
+            if (user.getId().equals(requestRemoveToken.getFkUserId())) {
+                throw new Exception(Constants.CAN_NOT_DELETE_TOKEN);
             }
-            SystemDao systemDao = SystemDao.getSystem(requestRemoveToken.getFkSystemId());
-            String privilegeName = "REMOVE_" + systemDao.getSystemName() + "_TOKENS";
-            user.checkPrivilege(privilegeName);
-            if (!transaction.isActive())
-                transaction.begin();
-            entityManager.createQuery("DELETE FROM TokenDao WHERE token = :token AND fkSystemId = :fkSystemId")
-                    .setParameter("token", requestRemoveToken.getToken())
+            SystemDao systemOfRemovingToken = SystemDao.getSystem(requestRemoveToken.getFkSystemId());
+            // TODO: 2018-10-18 باید به نحوی باشد که برود سیستم های کاربر را چک کند و درصورتیکه جزو سیستم ها بود حذف کند
+            PrivilegeDao privilege = PrivilegeDao.getPrivilege(PrivilegeNames.REMOVE_TOKEN, systemOfRemovingToken.getId());
+            user.checkPrivilege(privilege);
+
+            entityManager.createQuery("DELETE FROM TokenDao t WHERE t.fkUserId = :fkUserId AND t.fkSystemId = :fkSystemId")
+                    .setParameter("fkUserId", requestRemoveToken.getFkUserId())
                     .setParameter("fkSystemId", requestRemoveToken.getFkSystemId())
                     .executeUpdate();
             if (transaction.isActive())
