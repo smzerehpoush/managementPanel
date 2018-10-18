3478fdb74c191af4385ea3788cce56d904ea6185
Unknown <unknown@example.com>
2018-10-18 10:43:36 +0330

    change base model from abstract class to interface

--- src/com/nrdc/policeHamrah/model/dto/BaseModel.java
+++ src/com/nrdc/policeHamrah/model/dto/BaseModel.java
@@ -2,5 +2,5 @@ package com.nrdc.policeHamrah.model.dto;
 
 import java.io.Serializable;
 
-public abstract class BaseModel implements Serializable, Cloneable {
+public interface BaseModel extends Serializable, Cloneable {
 }
