cb991e292b004f5fbda4361f8ce480b199494ade
mahdiyar zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-20 19:01:58 +0330

    refactor project

--- src/com/nrdc/policeHamrah/service/LoginService.java
+++ src/com/nrdc/policeHamrah/service/LoginService.java
@@ -17,6 +17,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 //done
 @Path("/login")
 public class LoginService {
@@ -46,6 +47,7 @@ public class LoginService {
 
     /**
      * setvice to login into different systems
+     *
      * @param request {@link RequestLoginToSystems}
      * @return StandardResponse with {@link com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseLogin}
      */
--- src/com/nrdc/policeHamrah/service/LogoutService.java
+++ src/com/nrdc/policeHamrah/service/LogoutService.java
@@ -14,6 +14,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
 //done
 @Path("/logout")
 public class LogoutService {
