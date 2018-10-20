cd5abbd280a91da0092155e31e3deedc004e5974
mahdiyar zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-20 19:04:20 +0330

    add prototype of new role services impl

--- src/com/nrdc/policeHamrah/impl/RoleImpl.java
+++ src/com/nrdc/policeHamrah/impl/RoleImpl.java
@@ -2,6 +2,8 @@ package com.nrdc.policeHamrah.impl;
 
 import com.nrdc.policeHamrah.helper.PrivilegeNames;
 import com.nrdc.policeHamrah.jsonModel.StandardResponse;
+import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestAddRole;
+import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestEditRole;
 import com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseGetPrivileges;
 import com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseGetRoles;
 import com.nrdc.policeHamrah.model.dao.UserDao;
@@ -10,6 +12,21 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 public class RoleImpl {
+    public StandardResponse addRole(String token, RequestAddRole requestAddRole) {
+        // TODO: 10/20/2018 Implement add role service
+        return null;
+    }
+
+    public StandardResponse editRole(String token, RequestEditRole requestEditRole) {
+        // TODO: 10/20/2018 Implement edit role service
+        return null;
+    }
+
+    public StandardResponse removeRole(String token, Long fkRoleId) {
+        // TODO: 10/20/2018 Implement remove role service
+        return null;
+    }
+
     public StandardResponse getRoles(String token) throws Exception {
         EntityManager entityManager = Database.getEntityManager();
         try {
