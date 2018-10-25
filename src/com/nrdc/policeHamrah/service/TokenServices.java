e4405c6bcec41d69f1a0c4b70ef7918e929255bf
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-25 15:00:11 +0330

    change method arguments ...

--- src/com/nrdc/policeHamrah/service/TokenServices.java
+++ src/com/nrdc/policeHamrah/service/TokenServices.java
@@ -2,36 +2,34 @@ package com.nrdc.policeHamrah.service;
 
 import com.nrdc.policeHamrah.helper.Encryption;
 import com.nrdc.policeHamrah.impl.TokenImpl;
-import com.nrdc.policeHamrah.jsonModel.EncryptedRequest;
 import com.nrdc.policeHamrah.jsonModel.EncryptedResponse;
 import com.nrdc.policeHamrah.jsonModel.StandardResponse;
-import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestRemoveToken;
 import com.nrdc.policeHamrah.model.dao.UserDao;
 import org.apache.log4j.Logger;
-import org.codehaus.jackson.map.ObjectMapper;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/token")
 public class TokenServices {
     private static Logger logger = Logger.getLogger(TokenServices.class.getName());
-    private ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * @param token      user token
+     * @param fkSystemId id of system
+     * @param fkUserId   id of user
+     * @return simple StandardResponse to handle state
+     */
     @Path("/remove")
     @DELETE
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response removeToken(EncryptedRequest encryptedRequest) {
+    public Response removeToken(@QueryParam("token") String token, @QueryParam("fkSystemId") Long fkSystemId, @QueryParam("fkUserId") Long fkUserId) {
         logger.info("++================== removeToken SERVICE : START ==================++");
         try {
-            RequestRemoveToken request = objectMapper.readValue(Encryption.decryptRequest(encryptedRequest), RequestRemoveToken.class);
-            StandardResponse response = new TokenImpl().removeToken(encryptedRequest.getToken(), request);
-            String key = UserDao.getKey(encryptedRequest.getToken()).getKey();
+            StandardResponse response = new TokenImpl().removeToken(token, fkSystemId, fkUserId);
+            String key = UserDao.getKey(token).getKey();
             EncryptedResponse encryptedResponse = Encryption.encryptResponse(key, response);
             Response finalResponse = Response.status(200).entity(encryptedResponse).build();
             logger.info("++================== removeToken SERVICE : END ==================++");
