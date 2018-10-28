980fe7ebc95d7a2198c44930584ee12c57b7e99a
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-28 10:01:35 +0330

    edit method implementaion

--- src/com/nrdc/policeHamrah/impl/PrivilegeImpl.java
+++ src/com/nrdc/policeHamrah/impl/PrivilegeImpl.java
@@ -1,6 +1,5 @@
 package com.nrdc.policeHamrah.impl;
 
-import com.nrdc.policeHamrah.helper.PrivilegeNames;
 import com.nrdc.policeHamrah.jsonModel.StandardResponse;
 import com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseGetPrivileges;
 import com.nrdc.policeHamrah.model.dao.UserDao;
@@ -10,12 +9,18 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 public class PrivilegeImpl {
-    public StandardResponse<ResponseGetPrivileges> getPrivileges(String token) throws Exception {
+    public StandardResponse<ResponseGetPrivileges> getPrivileges(String token, Long fkSystemId) throws Exception {
         EntityManager entityManager = Database.getEntityManager();
         try {
             UserDao user = UserDao.validate(token);
-            user.checkPrivilege(PrivilegeNames.GET_PRIVILEGES);
-            List<PrivilegeDto> privileges = entityManager.createQuery("SELECT p FROM PrivilegeDao p")
+            List<PrivilegeDto> privileges = entityManager.createQuery("SELECT distinct (p) FROM PrivilegeDao p " +
+                    "JOIN RolePrivilegeDao rp ON p.id = rp.fkPrivilegeId " +
+                    "JOIN UserRoleDao ur ON rp.fkRoleId = ur.fkRoleId " +
+                    "JOIN AuthDao t ON ur.fkUserId = t.fkUserId " +
+                    "JOIN PrivilegeSystemDao ps ON p.id = ps.fkPrivilegeId  " +
+                    "WHERE ps.fkSystemId = :fkSystemId AND t.token= :token")
+                    .setParameter("token", token)
+                    .setParameter("fkSystemId", fkSystemId)
                     .getResultList();
             ResponseGetPrivileges responseGetPrivileges = new ResponseGetPrivileges();
             responseGetPrivileges.setPrivileges(privileges);
