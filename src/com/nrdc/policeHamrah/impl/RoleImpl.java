a32d52317e29f49905c36494ba29aa2bd8387fd4
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-10-28 10:12:58 +0330

    remove unnecessary function

--- src/com/nrdc/policeHamrah/impl/RoleImpl.java
+++ src/com/nrdc/policeHamrah/impl/RoleImpl.java
@@ -5,7 +5,6 @@ import com.nrdc.policeHamrah.jsonModel.StandardResponse;
 import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestAddRole;
 import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestEditRole;
 import com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseGetPrivileges;
-import com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseGetRoles;
 import com.nrdc.policeHamrah.model.dao.RoleDao;
 import com.nrdc.policeHamrah.model.dao.RolePrivilegeDao;
 import com.nrdc.policeHamrah.model.dao.UserDao;
@@ -109,32 +108,14 @@ public class RoleImpl {
         }
     }
 
-    public StandardResponse getSystemRoles(String token) throws Exception {
+
+    public StandardResponse getRolePrivileges(String token, Long fkRoleId, Long fkSystemId) throws Exception {
         EntityManager entityManager = Database.getEntityManager();
         try {
             UserDao user = UserDao.validate(token);
-            user.checkPrivilege(PrivilegeNames.GET_ROLES);
-            List roles = entityManager.createQuery("SELECT r FROM RoleDao r")
-                    .getResultList();
-            ResponseGetRoles responseGetRoles = new ResponseGetRoles();
-            responseGetRoles.setRoles(roles);
-            StandardResponse response = new StandardResponse<>();
-
-
-            response.setResponse(responseGetRoles);
-            return response;
-        } finally {
-            if (entityManager.isOpen())
-                entityManager.close();
-        }
-    }
-
-    public StandardResponse getRolePrivileges(String token, Long fkRoleId) throws Exception {
-        EntityManager entityManager = Database.getEntityManager();
-        try {
-            UserDao user = UserDao.validate(token);
-            user.checkPrivilege(PrivilegeNames.GET_PRIVILEGES);
+            List privileges = entityManager.createQuery("SELECT p FROM PrivilegeDao p JOIN RolePrivilegeDao rp ON p.id = rp.fkPrivilegeId JOIN PrivilegeSystemDao  ps ON p.id = ps.fkPrivilegeId WHERE ps.fkSystemId = :fkSystemId AND rp.fkRoleId = :fkRoleId")
                     .setParameter("fkRoleId", fkRoleId)
+                    .setParameter("fkSystemId", fkSystemId)
                     .getResultList();
             ResponseGetPrivileges responseGetPrivileges = new ResponseGetPrivileges();
             responseGetPrivileges.setPrivileges(privileges);
