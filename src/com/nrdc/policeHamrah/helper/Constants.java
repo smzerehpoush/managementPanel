662771f4f3cf108f11c81a930a4139d92835f4c7
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-11-18 15:15:44 +0330

    correct names and orders

--- src/com/nrdc/policeHamrah/helper/Constants.java
+++ src/com/nrdc/policeHamrah/helper/Constants.java
@@ -2,16 +2,25 @@ package com.nrdc.policeHamrah.helper;
 
 public class Constants {
     public static final String NOT_VALID_ROLE = "نقش وارد شده معتبر نمی باشد.";
-    public static final String NOT_VALID_REQUEST = "درخواست از سمت انئروید معتبر نمی باشد.";
-    public static final String NOT_LOGED_IN = "کاربر وارد سامانه مذکور نشده است.";
-    public static final String NOT_ACTIVE_USER = "چنین کاربری غیر فعال می باشد.لطفا کاربر را فعال کنید.";
-    public static final String CANT_DE_ACTIVE_YOURSELF = "نمی توانید خودتان را غیر فغال کنید.";
-    public static final String CAN_NOT_ASSIGN_THIE_ROLE = "شما نمی توانید نقش مدیر ارشد را به دیگران تخصیص دهید.";
-
     public static final String NOT_VALID_TOKEN = "توکن معتبر نیست.";
     public static final String NOT_VALID_SYSTEM = "سیستم معتبر نمی باشد. یا هنوز اضافه نشده است.";
     public static final String NOT_VALID_USER = "کاربر معتبر نمی باشد";
     public static final String NOT_VALID_PRIVILEGE = "دسترسی معتبر نمی باشد";
+
+    public static final String NOT_VALID_REQUEST = "درخواست از سمت انئروید معتبر نمی باشد.";
+    public static final String NOT_LOGED_IN = "کاربر وارد سامانه مذکور نشده است.";
+    public static final String NOT_ACTIVE_USER = "چنین کاربری غیر فعال می باشد.لطفا کاربر را فعال کنید.";
+
+    public static final String CAN_NOT_DE_ACTIVE_YOURSELF = "نمی توانید خودتان را غیر فغال کنید.";
+    public static final String CAN_NOT_ASSIGN_THIE_ROLE = "شما نمی توانید نقش مدیر ارشد را به دیگران تخصیص دهید.";
+    public static final String CAN_NOT_DELETE_TOKEN = "شما نمی توانید توکن خودتان را حذف کنید.";
+    public static final String CAN_NOT_RETRIEVE_DATA_FROM_NAJA = "ایراد در دریافت اطلاعات از اجرائیات.";
+    public static final String CAN_NOT_EDIT_SYSADMIN = "شما نمی توانید نقش مدیر ارشد را تغییر دهید";
+    public static final String CAN_NOT_CREATE_SYSADMIN = "شما نمی توانید نقش مدیر ارشد را تعریف کنید. نقشی که همه دسترسی ها را شامل شود.";
+    public static final String CAN_NOT_REMOVE_SYSADMIN = "شما نمی توانید نقش مدیر ارشد را حذف کنید";
+    public static final String CAN_NOT_EDIT_THIS_USER = "شما دسترسی تغییر کارری با چنین سیتمی را ندارید.";
+    public static final String CAN_NOT_DE_ACTIVE_THIS_USER = "شما دسترسی غیر فعال کردن کارری با چنین سیتمی را ندارید.";
+
 
     public static final String NO_USER_FOR_TOKEN = "کاربری بر اساس توکن داده شده پیدا نشد.";
     public static final String USER_IS_NOT_ACTIVE = "کاربر فعال نمی باشد";
@@ -20,24 +29,12 @@ public class Constants {
     public static final String UNKNOWN_ERROR = "اشتباهی رخ داده است. دوباره تلاش کنید.";
     public static final String UNKNOWN_USER = "چنین کاربری وجود ندارد.";
 
-    public static final String CAN_NOT_DELETE_TOKEN = "شما نمی توانید توکن خودتان را حذف کنید.";
-    public static final String CAN_NOT_RETRIEVE_DATA_FROM_NAJA = "ایراد در دریافت اطلاعات از اجرائیات.";
-    public static final String CAN_NOT_CHANGE_YOUR_ROLE = "شما نمی توانید درجه خود را تغییر دهید.";
-    public static final String NON_UNIQUE_ROLE = "نقش غیر یکتا";
-
     public static final String VT_CONNECTION_ERROR = "ایراد در ارتباط با سرور ثبت تخلفات .";
 
     public static final String PERMISSION_ERROR = "شما دسترسی استفاده از این خدمت را ندارید.";
 
-    public static final String SCHEMA = "POLICE_HAMRAH";
-    public static final String CANT_NOT_EDIT_THIS_USER = "شما دسترسی تغییر کارری با چنین سیتمی را ندارید.";
-    public static final String CANT_NOT_ACTIVE_THIS_USER = "شما دسترسی فعال کردن کارری با چنین سیتمی را ندارید.";
-    public static final String CANT_NOT_DEACTIVE_THIS_USER = "شما دسترسی غیر فعال کردن کارری با چنین سیتمی را ندارید.";
     public static final String USER_SYSTEM_ERROR = "کاربر مربوط به سیستم وارد شده نمی باشد.";
-    public static final String CAN_NOT_REMOVE_YOUR_TOKEN = "نمی توانید توکن خود را حذف کنید";
-    public static final String CAN_NOT_EDIT_SYSADMIN = "شما نمی توانید نقش مدیر ارشد را تغییر دهید";
-    public static final String CAN_NOT_CREATE_SYSADMIN = "شما نمی توانید نقش مدیر ارشد را تعریف کنید. نقشی که همه دسترسی ها را شامل شود.";
-    public static final String CANT_NOT_REMOVE_SYSADMIN = "شما نمی توانید نقش مدیر ارشد را حذف کنید";
+    public static final String SCHEMA = "POLICE_HAMRAH";
     public static final String SYS_ADMIN = "SYS_ADMIN";
 
 }
