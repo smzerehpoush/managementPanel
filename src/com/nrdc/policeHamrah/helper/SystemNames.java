9c35b7a82f951b4696c4c8af9876df7eb228af66
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-11-28 18:20:52 +0330

    add gashti system name

--- src/com/nrdc/policeHamrah/helper/SystemNames.java
+++ src/com/nrdc/policeHamrah/helper/SystemNames.java
@@ -3,5 +3,5 @@ package com.nrdc.policeHamrah.helper;
 public enum SystemNames {
     //danger!
     //do not change this file!!!!
-    VEHICLE_TICKET, NAZER, POLICE_HAMRAH
+    VEHICLE_TICKET, NAZER, POLICE_HAMRAH, GASHT
 }
