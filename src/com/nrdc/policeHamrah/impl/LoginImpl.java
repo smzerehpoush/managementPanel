7f4ca13b336dbf87c01ac1ac7fb9ea448db7cf78
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-12-26 14:17:05 +0330

    impl new services : authenticateUser checkUserInPh fillRequestBySystemId

--- src/com/nrdc/policeHamrah/impl/LoginImpl.java
+++ src/com/nrdc/policeHamrah/impl/LoginImpl.java
@@ -1,11 +1,8 @@
 package com.nrdc.policeHamrah.impl;
 
-import com.nrdc.policeHamrah.helper.Constants;
-import com.nrdc.policeHamrah.helper.Encryption;
-import com.nrdc.policeHamrah.helper.PrivilegeNames;
-import com.nrdc.policeHamrah.helper.SystemNames;
+import com.nrdc.policeHamrah.helper.*;
 import com.nrdc.policeHamrah.jsonModel.StandardResponse;
-import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestAddTokenKey;
+import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestAuthenticateUser;
 import com.nrdc.policeHamrah.jsonModel.jsonRequest.RequestLogin;
 import com.nrdc.policeHamrah.jsonModel.jsonResponse.ResponseLogin;
 import com.nrdc.policeHamrah.model.dao.*;
@@ -17,14 +14,147 @@ import org.codehaus.jackson.map.ObjectMapper;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import javax.persistence.NoResultException;
 
 public class LoginImpl {
     private static Logger logger = Logger.getLogger(LoginImpl.class.getName());
+
+    private StandardResponse authenticateVTUser(String policeCode, String password) {
+        EntityManager entityManager = Database.getEntityManager();
+        entityManager.getEntityManagerFactory().getCache().evictAll();
+        StandardResponse response = new StandardResponse();
+        try {
+            try {
+                UserDao user = (UserDao) entityManager.createQuery("SELECT u FROM UserDao u WHERE u.policeCode = :policeCode")
+                        .setParameter("policeCode", policeCode)
+                        .getSingleResult();
+                if (!user.getPassword().equals(password)) {
+                    response.setResultCode(-1);
+                    response.setResultMessage(Constants.INCORRECT_USERNAME_OR_PASSWORD);
+                }
+            } catch (NoResultException ex) {
+                response.setResultCode(-1);
+                response.setResultMessage(Constants.UNKNOWN_USER);
+
+            } catch (Exception ex) {
+                response.setResultCode(-1);
+                response.setResultMessage(Constants.NOT_VALID_USER);
+            }
+        } finally {
+            if (entityManager.isOpen())
+                entityManager.close();
+            return response;
+
+        }
+    }
+
+    private void checkUserInPh(RequestAuthenticateUser requestAuthenticateUser) throws Exception {
+        SystemDao systemDao = SystemDao.getSystem(requestAuthenticateUser.getFkSystemId());
+        Long size = 0L;
+        EntityManager entityManager = Database.getEntityManager();
+        entityManager.getEntityManagerFactory().getCache().evictAll();
+        try {
+            if (systemDao.getSystemName().equals(SystemNames.NAZER.name())) {
+                size = (Long) entityManager.createQuery("SELECT COUNT (u) FROM UserDao u WHERE u.phoneNumber = :phoneNumber ")
+                        .setParameter("phoneNumber", requestAuthenticateUser.getPhoneNumber())
+                        .getSingleResult();
+
+            } else if (systemDao.getSystemName().equals(SystemNames.VEHICLE_TICKET.name())) {
+                size = (Long) entityManager.createQuery("SELECT COUNT (u) FROM UserDao u WHERE u.policeCode = :policeCode ")
+                        .setParameter("policeCode", requestAuthenticateUser.getPhoneNumber())
+                        .getSingleResult();
+            }
+            if (size.compareTo(0L) > 0)
+                throw new Exception(Constants.YOU_HAVE_PH_USER);
+
+        } finally {
+            if (entityManager.isOpen())
+                entityManager.close();
+        }
+    }
+
+    public StandardResponse authenticateUser(RequestAuthenticateUser requestAuthenticateUser) throws Exception {
+        checkUserInPh(requestAuthenticateUser);
+        SystemDao systemDao = SystemDao.getSystem(requestAuthenticateUser.getFkSystemId());
+        Object request = fillRequestBySystemId(systemDao.getSystemName(), requestAuthenticateUser);
+        String output = CallWebService.callPostService(systemDao.getSystemPath() + "/authenticateUser", request);
+        StandardResponse response = new ObjectMapper().readValue(output, StandardResponse.class);
+        if (response.getResultCode() == -1) {
+            if (response.getResultMessage().trim().equals("1"))
+                response.setResultMessage(Constants.UNKNOWN_USER);
+            else if (response.getResultMessage().trim().equals("2"))
+                response.setResultMessage(Constants.INCORRECT_USERNAME_OR_PASSWORD);
+            else if (response.getResultMessage().trim().equals("3"))
+                response.setResultMessage(Constants.NAZER_NOT_APN_USER);
+        }
+        return response;
+    }
+
+    private Object fillRequestBySystemId(String systemName, RequestAuthenticateUser requestAuthenticateUser) throws Exception {
+        if (systemName.equals(SystemNames.NAZER.name())) {
+            RequestAuthenticateNazer requestAuthenticateNazer = new RequestAuthenticateNazer();
+            requestAuthenticateNazer.setPhoneNumber(requestAuthenticateUser.getPhoneNumber());
+            requestAuthenticateNazer.setPassword(requestAuthenticateUser.getPassword());
+            return requestAuthenticateNazer;
+        } else if (systemName.equals(SystemNames.VEHICLE_TICKET.name())) {
+            RequestAuthenticateVT requestAuthenticateVT = new RequestAuthenticateVT();
+            requestAuthenticateVT.setPoliceCode(requestAuthenticateUser.getPoliceCode());
+            requestAuthenticateVT.setPassword(requestAuthenticateUser.getPassword());
+            return requestAuthenticateVT;
+        }
+        throw new Exception(Constants.NOT_VALID_SYSTEM);
+    }
+
+    private class RequestAuthenticateVT {
+        private String policeCode;
+        private String password;
+
+        public String getPoliceCode() {
+            return policeCode;
+        }
+
+        public void setPoliceCode(String policeCode) {
+            this.policeCode = policeCode;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+
+    private class RequestAuthenticateNazer {
+        private String phoneNumber;
+        private String password;
+
+        public String getPhoneNumber() {
+            return phoneNumber;
+        }
+
+        public void setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String toString() {
+            final StringBuilder sb = new StringBuilder("RequestAuthenticateNazer{");
+            sb.append("phoneNumber='").append(phoneNumber).append('\'');
+            sb.append(", password='").append(password).append('\'');
+            sb.append('}');
+            return sb.toString();
+        }
+    }
 
     /**
      * @param requestLogin username, phoneNumber, password to login into Police Hamrah
@@ -69,7 +199,7 @@ public class LoginImpl {
             responseLogin.setKey(auth.getKey());
             responseLogin.setToken(auth.getToken());
             responseLogin.setUser(user.createCustomUser());
-            StandardResponse response = new StandardResponse();
+            StandardResponse<ResponseLogin> response = new StandardResponse<>();
             response.setResponse(responseLogin);
             operation.setFkUserId(user.getId());
             operation.setStatusCode(1L);
@@ -92,7 +222,14 @@ public class LoginImpl {
         }
     }
 
-    public StandardResponse<ResponseLogin> loginToSystem(String token, Long fkSystemId) throws Exception {
+    /***
+     * this service calls by other servers like nazer, agahi,...
+     * @param policeHamrahToken token of user in policeHamrah system
+     * @param fkSystemId id of system that user wants to login to it
+     * @return ResponseLogin
+     * @throws Exception
+     */
+    public StandardResponse<ResponseLogin> loginToSystem(String policeHamrahToken, Long fkSystemId) throws Exception {
         EntityManager entityManager = Database.getEntityManager();
         EntityTransaction transaction = entityManager.getTransaction();
         entityManager.getEntityManagerFactory().getCache().evictAll();
@@ -101,10 +238,10 @@ public class LoginImpl {
             if (transaction != null && !transaction.isActive()) {
                 transaction.begin();
             }
-            UserDao user = UserDao.getUser(token);
-            user.isActive();
+            UserDao user = UserDao.validate(policeHamrahToken);
+            if (!user.getIsActive())
+                throw new Exception(Constants.USER_IS_NOT_ACTIVE);
             SystemDao systemDao = SystemDao.getSystem(fkSystemId);
-            user.checkPrivilege(PrivilegeNames.LOGIN, fkSystemId);
             StandardResponse<ResponseLogin> response = loginToSystem(user, systemDao);
             if (transaction != null && transaction.isActive())
                 transaction.commit();
@@ -123,10 +260,6 @@ public class LoginImpl {
         user.checkKey(systemDao);
         user.checkToken(systemDao);
         AuthDao auth = new AuthDao(user, systemDao);
-        StandardResponse responseAddTokenKey = sendTokenKeyToSystem(user, auth, systemDao);
-        if (responseAddTokenKey.getResultCode() == -1) {
-            throw new Exception(responseAddTokenKey.getResultMessage());
-        }
         EntityManager entityManager = Database.getEntityManager();
         entityManager.getEntityManagerFactory().getCache().evictAll();
         EntityTransaction transaction = entityManager.getTransaction();
@@ -136,8 +269,7 @@ public class LoginImpl {
             entityManager.persist(auth);
             if (transaction != null && transaction.isActive())
                 transaction.commit();
-            StandardResponse<ResponseLogin> response = createResponseLogin(user, systemDao, auth);
-            return response;
+            return createResponseLogin(user, systemDao, auth);
 
         } catch (Exception ex) {
             if (transaction != null && transaction.isActive())
@@ -167,34 +299,33 @@ public class LoginImpl {
         return response;
     }
 
-    private StandardResponse sendTokenKeyToSystem(UserDao user, AuthDao auth, SystemDao systemDao) throws Exception {
-        RequestAddTokenKey requestAddTokenKey = new RequestAddTokenKey();
-        requestAddTokenKey.setKey(auth.getKey());
-        requestAddTokenKey.setToken(auth.getToken());
-        requestAddTokenKey.setNationalId(user.getNationalId());
-        requestAddTokenKey.setPoliceCode(user.getPoliceCode());
-        requestAddTokenKey.setPhoneNumber(user.getPhoneNumber());
-        URL url = new URL(systemDao.getSystemPath() + "/addTokenKey");
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
-        connection.setRequestProperty("Content-Type", "application/json");
-        String input = new ObjectMapper().writeValueAsString(requestAddTokenKey);
-        OutputStream outputStream = connection.getOutputStream();
-        outputStream.write(input.getBytes());
-        outputStream.flush();
-        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            throw new Exception("Can not add token to system : " + connection.getResponseCode());
-        }
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder output = new StringBuilder();
-        int c;
-        while ((c = bufferedReader.read()) != -1)
-            output.append((char) c);
-        return new ObjectMapper().readValue(output.toString(), StandardResponse.class);
-
-
-    }
+//    private StandardResponse sendTokenKeyToSystem(UserDao user, AuthDao auth, SystemDao systemDao) throws Exception {
+//        RequestAddTokenKey requestAddTokenKey = new RequestAddTokenKey();
+//        requestAddTokenKey.setKey(auth.getKey());
+//        requestAddTokenKey.setToken(auth.getToken());
+//        requestAddTokenKey.setNationalId(user.getNationalId());
+//        requestAddTokenKey.setPoliceCode(user.getPoliceCode());
+//        requestAddTokenKey.setPhoneNumber(user.getPhoneNumber());
+//        URL url = new URL(systemDao.getSystemPath() + "/addTokenKey");
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//        connection.setDoOutput(true);
+//        connection.setRequestMethod("POST");
+//        connection.setRequestProperty("Content-Type", "application/json");
+//        String input = new ObjectMapper().writeValueAsString(requestAddTokenKey);
+//        OutputStream outputStream = connection.getOutputStream();
+//        outputStream.write(input.getBytes());
+//        outputStream.flush();
+//        if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+//            throw new Exception("Can not add token to system : " + connection.getResponseCode());
+//        }
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+//        StringBuilder output = new StringBuilder();
+//        int c;
+//        while ((c = bufferedReader.read()) != -1)
+//            output.append((char) c);
+//        return new ObjectMapper().readValue(output.toString(), StandardResponse.class);
+//
+//    }
 
     private UserDao verifyUser(RequestLogin requestLogin) throws Exception {
         String username = requestLogin.getUsername();
