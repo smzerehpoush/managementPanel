f9e8d43b97169d8534e80333ed8274c575f845bf
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2018-12-26 13:31:09 +0330

    add new Server Exception that automatically creates a server

--- src/com/nrdc/policeHamrah/exceptions/ServerException.java
+++ src/com/nrdc/policeHamrah/exceptions/ServerException.java
@@ -11,12 +11,24 @@ import javax.ws.rs.core.Response;
 public class ServerException extends Exception {
     private static Logger logger = Logger.getLogger(ServerException.class.getName());
 
-    public static Response create(String message, Exception ex, String token) throws Exception {
+    public static Response create(String message, Exception ex, String token) {
         logger.error(message);
-        logger.error(ex.getMessage(), ex);
+        logger.error(ex.getMessage());
         StandardResponse response = StandardResponse.getNOKExceptions(ex);
-        String key = UserDao.getKey(token).getKey();
+        String key;
+        try {
+            key = UserDao.getKey(token).getKey();
+        } catch (Exception e) {
+            key = "GOFYS";
+        }
         EncryptedResponse encryptedResponse = Encryption.encryptResponse(key, response);
         return Response.status(200).entity(encryptedResponse).build();
+    }
+
+    public static Response create(String message, Exception ex) {
+        logger.error(message);
+        logger.error(ex.getMessage());
+        StandardResponse response = StandardResponse.getNOKExceptions(ex);
+        return Response.status(200).entity(response).build();
     }
 }
