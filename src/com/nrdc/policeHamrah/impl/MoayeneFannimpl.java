1e6c6fac4e7ccae59fc6159395aaeffcfc1eb222
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-02-27 09:49:34 +0330

    add new moayene fanni service

---
+++ src/com/nrdc/policeHamrah/impl/MoayeneFannimpl.java
@@ -0,0 +1,9 @@
+package com.nrdc.policeHamrah.impl;
+
+import com.nrdc.policeHamrah.jsonModel.StandardResponse;
+
+public class MoayeneFannimpl {
+    public StandardResponse getInfo(String plateNo) {
+        return null;
+    }
+}
