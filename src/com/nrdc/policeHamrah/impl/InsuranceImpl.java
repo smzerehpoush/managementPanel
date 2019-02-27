c615e7d4a07f3a6811bdad3df94565b191c23761
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-02-27 09:49:10 +0330

    add new service : insurance

---
+++ src/com/nrdc/policeHamrah/impl/InsuranceImpl.java
@@ -0,0 +1,9 @@
+package com.nrdc.policeHamrah.impl;
+
+import com.nrdc.policeHamrah.jsonModel.StandardResponse;
+
+public class InsuranceImpl {
+    public StandardResponse getInfo(String plateNo) {
+        return null;
+    }
+}
