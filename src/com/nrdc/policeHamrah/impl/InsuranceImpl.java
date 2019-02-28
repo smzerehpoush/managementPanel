3af574b5b83dbe749616c04a3e312a8404ba02d1
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-02-28 12:08:13 +0330

    remove services

--- src/com/nrdc/policeHamrah/impl/InsuranceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.nrdc.policeHamrah.impl;
-
-import com.nrdc.policeHamrah.jsonModel.StandardResponse;
-
-public class InsuranceImpl {
-    public StandardResponse getInfo(String plateNo) {
-        return null;
-    }
-}
--- src/com/nrdc/policeHamrah/impl/MoayeneFannimpl.java
+++
@@ -1,9 +0,0 @@
-package com.nrdc.policeHamrah.impl;
-
-import com.nrdc.policeHamrah.jsonModel.StandardResponse;
-
-public class MoayeneFannimpl {
-    public StandardResponse getInfo(String plateNo) {
-        return null;
-    }
-}
