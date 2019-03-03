7790ddaafea50b1536657e8c8766c97d5a831688
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-03-03 15:49:31 +0330

    add crashes to system names

--- src/com/nrdc/policeHamrah/helper/SystemNames.java
+++ src/com/nrdc/policeHamrah/helper/SystemNames.java
@@ -3,5 +3,5 @@ package com.nrdc.policeHamrah.helper;
 public enum SystemNames {
     //danger!
     //do not change this file!!!!
-    VEHICLE_TICKET, NAZER, POLICE_HAMRAH, GASHT,AGAHI,VT_REPORT
+    VEHICLE_TICKET, NAZER, POLICE_HAMRAH, GASHT,AGAHI,VT_REPORT, CRASHES
 }
