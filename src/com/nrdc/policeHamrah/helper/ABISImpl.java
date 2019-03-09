bd5bb022b6554b1fa6d5fc2a25c00abd4c2e3c45
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2019-03-09 09:28:00 +0330

    implement abis service

--- src/com/nrdc/policeHamrah/helper/ABISImpl.java
+++ src/com/nrdc/policeHamrah/helper/ABISImpl.java
@@ -5,8 +5,16 @@ import com.nrdc.policeHamrah.model.dao.ConstantDao;
 
 public class ABISImpl {
     public StandardResponse sendRequest(Object request, String serviceName) throws Exception {
-        String servicePath = ConstantDao.getConstant(serviceName);
+        String servicePath = ConstantDao.getConstant("ABIS_BASE_PATH") + ConstantDao.getConstant(serviceName);
         String result = CallWebService.callPostService(servicePath, request);
+        StandardResponse<String> response = new StandardResponse<>();
+        response.setResponse(result);
+        return response;
+    }
+
+    public StandardResponse sendRequest(String serviceName, String pathParam) throws Exception {
+        String servicePath = ConstantDao.getConstant("ABIS_BASE_PATH") + ConstantDao.getConstant(serviceName);
+        String result = CallWebService.callGetService(servicePath + "/" + pathParam);
         StandardResponse<String> response = new StandardResponse<>();
         response.setResponse(result);
         return response;
